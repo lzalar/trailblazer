ae3b8ad1f53876035d933604954170fe598f5909
lzalar <leonardo.zalar@tacta.io>
2024-05-09 00:14:19 +0200

    insert initial users

--- raceproducer/src/main/resources/db/migration/V1_1_0__initial_schema.sql
+++ raceproducer/src/main/resources/db/migration/V1_1_0__initial_schema.sql
@@ -26,3 +26,8 @@ create table race_application
 
 create index idx_race_application_race_id on race_application (race_id);
 create index idx_race_application_user_id on race_application (user_id);
+
+insert into app_user
+values ('baa39cd6-e9f0-4d13-994c-c2b8863c4b3b', 'ivan', 'horvat', 'ivan.horvat@traiblazer.com', '1997-05-15');
+insert into app_user
+values ('65bba92e-e226-4514-8d9a-70af551dbe10', 'admin', 'admin', 'admin.admin@traiblazer.com', '1997-06-25');
